acef02eabe8b48b1479093d833078dcc0020e500
Brendo Oliveira <brendo.dev@outlook.com>
2023-04-24 20:44:41 -0300

    /

--- themes/cache/62115d9d7681b29777d7276482ae55feca248e4a.php
+++
@@ -1,5 +0,0 @@
-
-<?php $__env->startSection('content'); ?>
-    Brdev Framework.
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('web.theme', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\blogger\themes\views/web/welcome.blade.php ENDPATH**/ ?>
